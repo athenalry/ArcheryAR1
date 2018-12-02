90bc77f2b07eb43a70057f165e5abba35c60cec9
codeislife1314 <athenaparthenos2@gmail.com>
2018-12-01 19:48:27 -0500

    Initial commit

---
+++ Assets/arrow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class arrow : MonoBehaviour {
+    public Text t;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("target")) {
+            t.text = " HIT";
+        }
+    }
+}
---
+++ Assets/shoot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class shoot : MonoBehaviour {
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        //rb.useGravity = true;
+	}
+    public void restart()
+    {
+        rb.useGravity = false;
+        rb.transform.position = new Vector3(0, 0, 0);
+    }
+
+    float x=0, y = 0;
+	// Update is called once per frame
+	void Update () {
+        Touch t = Input.GetTouch(0);
+        switch (t.phase) {
+            case TouchPhase.Began:
+            y = t.position.y;
+            x = t.position.x;
+            break;
+            case TouchPhase.Ended:
+                rb.useGravity = true;
+                // rb.AddForce(t.position.x - x, Mathf.Abs(t.position.y - y) * 100, Mathf.Abs(t.position.y - y) * 100);
+                rb.velocity = new Vector3(-Mathf.Abs(t.position.y - y), -Mathf.Abs(t.position.y - y)/10000000);
+            break;
+        }
+	}
+
+}
